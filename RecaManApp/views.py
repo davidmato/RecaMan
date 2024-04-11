60c4248db5f537c000ed00eacb462fc0cea776ec
david mato <dmatodelalozapalomo@safareyes.es>
2024-04-11 17:52:00 +0200

    commit de views de ruben

--- RecaManApp/views.py
+++ RecaManApp/views.py
@@ -10,3 +10,28 @@ def nav(request):
 
 def footer(request):
     return render(request, 'footer.html')
+
+def areaboss(request):
+    return render(request, 'Area_Admin.html')
+
+
+def plantillamecanic(request):
+    list_mecanic = Mecanico.objects.all()
+    return render(request, 'PlantillaMecanico.html',{'mecanico': list_mecanic})
+
+
+def new_meacanic(request):
+
+    if request.method == 'GET':
+        return render(request, 'Area_Admin.html')
+    else:
+
+        nuevo = Mecanico()
+        nuevo.nombre = request.POST.get('mecanicnamen')
+        nuevo.email = request.POST.get('mail')
+        nuevo.fecha_nacimiento = request.POST.get('birth')
+        nuevo.dni = request.POST.get('dni')
+        nuevo.url = request.POST.get('url')
+        nuevo.save()
+
+        return redirect('/recaman/jefe/plantilla')
