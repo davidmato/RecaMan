8e7f5691159944bed1e3d56e0fd364dde0a2bd06
Mario Castro <mcrcdr@gmail.com>
2024-04-12 19:47:38 +0200

    pagina citas con añadir cita y su redirigir

--- RecaMan/urls.py
+++ RecaMan/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from RecaManApp.views import new_cita
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- RecaManApp/url.py
+++ RecaManApp/url.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('header/', header,name='header'),
     path('citas/', show_citas,name='citas'),
+    path('citas/new_cita/', new_cita,name='nueva_cita'),
 ]
