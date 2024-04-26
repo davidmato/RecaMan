383e845c48b16b51bd229392c0ea23dc1ef9339f
Mario Castro <mcrcdr@gmail.com>
2024-04-26 16:54:51 +0200

    arreglo de proyectos: url.py (sin terminar)

--- RecaManApp/url.py
+++ RecaManApp/url.py
@@ -3,5 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('header/', header, name='header'),
-    path('citas/', show_citas, name='citas'),
+    path('jefe/', areaboss),
+    path('jefe/citas/', show_citas, name='citas'),
+    path('jefe/newmecanic/', new_meacanic, name="newMecanic"),
+    path('jefe/plantilla/', plantillamecanic, name="plantillaMecanico"),
+    path('jefe/delete_mecanic/<int:id>', delete_mecanic, name="deleteMecanic"),
+    path('jefe/edit_mecanic/<int:id>', edit_mecanic, name="editMecanic"),
+    path('register', registrar_user, name="register")
 ]
