0ab2d0c0aa325aa474f66cd45f804cdfb77a165c
Mario Castro <mcrcdr@gmail.com>
2024-04-26 15:33:31 +0200

    commit despues de eliminar la pantalla añadir citas de la view del jefe

--- RecaManApp/url.py
+++ RecaManApp/url.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('header/', header,name='header'),
-    path('citas/', show_citas,name='citas'),
-    path('citas/new_cita/', new_cita,name='nueva_cita'),
+    path('header/', header, name='header'),
+    path('citas/', show_citas, name='citas'),
 ]
--- RecaManApp/views.py
+++ RecaManApp/views.py
@@ -6,22 +6,6 @@ def header(request):
     return render(request, 'header.html')
 def show_citas(request):
     list_citas = citas.objects.all()
-    return render(request, 'citas_jefe.html', {'citas': list_citas})
+    return render(request, 'listado_citas.html', {'citas': list_citas})
 
-def new_cita(request):
-    if request.method == 'GET':
-        lista_coches = CocheCliente.objects.all()
-        lista_mecanicos = mecanico.objects.all()
-        lista_clientes = Usuario.objects.all()
-        return render(request, 'citas_jefe.html', {'coches': lista_coches, 'mecanicos': lista_mecanicos, 'clientes': lista_clientes})
-    else:
-        new = citas()
-        new.fecha = request.POST['fecha']
-        new.hora = request.POST['hora']
-        new.motivo = request.POST['motivo']
-        new.cocheCliente = CocheCliente.objects.get(id=request.POST['coche'])
-        new.mecanico = mecanico.objects.get(id=request.POST['mecanico'])
-        new.usuario = Usuario.objects.get(id=request.POST['cliente'])
-        new.save()
 
-        return redirect('nueva_cita')
